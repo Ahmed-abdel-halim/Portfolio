524879cabd5a4b5a2d169e70f8285ee4b3c69d4b
Ahmed-abdel-halim <ahmedholim42@gmail.com>
2025-08-16 15:48:14 +0300

    edit

--- script.js
+++ script.js
@@ -132,3 +132,27 @@ if (typeof AOS !== 'undefined') {
             icon.classList.replace('fa-sun', 'fa-moon'); // قمر
         }
     });
+
+// certificate modal
+ var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: { slidesPerView: 1 },
+      576: { slidesPerView: 2 },
+      992: { slidesPerView: 3 }
+    }
+  });
